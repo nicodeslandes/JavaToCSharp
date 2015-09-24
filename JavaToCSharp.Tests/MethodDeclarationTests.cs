529ce346b2c7b3e6498a781d13065c78cfa6ab8f
Nicolas Deslandes <ndeslandes@live.fr>
2015-09-24 19:37:55 +0100

    Added unit tests

---
+++ JavaToCSharp.Tests/MethodDeclarationTests.cs
@@ -0,0 +1,44 @@
+﻿using com.github.javaparser;
+using com.github.javaparser.ast.body;
+using JavaToCSharp.Declarations;
+using JavaToCSharp.Statements;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Xunit;
+
+namespace JavaToCSharp.Tests
+{
+    public class MethodDeclarationTests
+    {
+        [Fact]
+        public void TestSimpleMethodDeclaration()
+        {
+            var java = @"public static void write() {}";
+            var csharp = @"public static void Write()
+{
+}";
+
+            var converted = ConvertStatement(java);
+            Assert.Equal(csharp, converted);
+        }
+
+        private static string ConvertStatement(string java)
+        {
+            var javaClassDeclaration = @"
+            class A
+            {
+                " + java + @"
+            }";
+            var declaration = JavaParser.parseBodyDeclaration(javaClassDeclaration);
+            var options = new JavaConversionOptions();
+            var context = new ConversionContext(options);
+            var classDeclaration = SyntaxFactory.ClassDeclaration("A");
+            var statementSyntax = BodyDeclarationVisitor.VisitBodyDeclarationForClass(context,
+                classDeclaration, (BodyDeclaration)declaration.getChildrenNodes().get(0))
+                .NormalizeWhitespace();
+
+            var tree = CSharpSyntaxTree.Create(statementSyntax);
+            return tree.GetText().ToString();
+        }
+    }
+}
